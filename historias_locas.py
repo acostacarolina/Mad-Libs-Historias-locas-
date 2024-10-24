bbfe42eef6dd97f9705028fc80a83cda35f21fa6
Carolina <caritos22@hotmail.com>
2024-10-24 20:19:55 -0300

    Primer commit

---
+++ historias_locas.py
@@ -0,0 +1,12 @@
+"""
+Reemplazar valores en una cadena de caracteres.
+"""
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo (plural): ")
+
+madlib = f"Programar es tan {adj}! Siempre me emocionda porque me encanta {verbo1} en Instituto La Celeste. Aprende a {verbo2} y alcanza tus {sustantivo_plural}!"
+
+print(madlib)
